641138843dd1e6df8f419c8d10f98df3c756db56
mma200631 <assumpta.chidinma06@gmail.com>
2026-09-10 22:50:13 +0100

    Add Swagger API documentation

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import route from './src/router.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' with { type: 'json' };
 const app= express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(route);
 
 app.get('/', (req, res)=> {
--- src/router.js
+++ src/router.js
@@ -3,7 +3,44 @@ const route= express.Router();
 
 import { getBooksHandler, getBookByIdHandler } from './controllers/books.js';
 
+/**
+ * @openapi
+ * /books:
+ *   get:
+ *     summary: Get all books
+ *     tags:
+ *       - Books
+ *     responses:
+ *       200:
+ *         description: A list of books
+ *       500:
+ *         description: Internal server error
+ */
 route.get('/books', getBooksHandler);
+
+
+/**
+ * @openapi
+ * /books/{id}:
+ *   get:
+ *     summary: Get a book by ID
+ *     tags:
+ *       - Books
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the book
+ *     responses:
+ *       200:
+ *         description: The requested book
+ *       404:
+ *         description: Book not found
+ *       500:
+ *         description: Internal server error
+ */
 route.get('/books/:id', getBookByIdHandler);
 
 export default route;
